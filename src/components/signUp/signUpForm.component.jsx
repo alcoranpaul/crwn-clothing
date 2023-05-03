2a12fe2cfcbad3710e81e7f5747f924446447e0d
Paul Adrian <paulreyes74@yahoo.com>
2023-05-02 23:32:00 -0500

    Added state for sign up form fields

---
+++ src/components/signUp/signUpForm.component.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"; // import the useState hook from the React library
+
+// set up an object with default values for the form fields
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+
+// define the SignUpForm component
+function SignUpForm() {
+  // use the useState hook to create state variables for the form fields
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  // destructure the form fields from the state object
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  // define a function that handles changes to the form fields
+  function handleChange(event) {
+    // destructure the name and value of the changed field from the event object
+    const { name, value } = event.target;
+
+    // Use the spread operator to copy the existing form fields and update the value of the field with the given name
+    setFormFields({ ...formFields, [name]: value });
+  }
+
+
+  // return the form JSX
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input type='text' required onChange={handleChange} name='displayName' value={displayName}/>
+
+        <label>Email</label>
+        <input type='email' required onChange={handleChange} name='email' value={email}/>
+
+        <label>Password</label>
+        <input type='password' required onChange={handleChange} name='password' value={password}/>
+
+        <label>Confirm Password</label>
+        <input type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
+
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUpForm; // export the SignUpForm component as the default export
--- src/routes/sign-in/signin.component.jsx
+++ src/routes/sign-in/signin.component.jsx
@@ -1,10 +1,11 @@
 import { 
     signInWithGooglePopup,
     createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
-
-
+import SignUpForm from "../../components/signUp/signUpForm.component";
 
 function SignIn(){
+
+
     async function logGoogleUser(){
         // signInWithGooglePopup is a Firebase method that allows users to sign in with their Google account using a popup window.
         const {user} = await signInWithGooglePopup();
@@ -16,6 +17,7 @@ function SignIn(){
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In with Google</button>
+            <SignUpForm/>
         </div>
     );
 }
