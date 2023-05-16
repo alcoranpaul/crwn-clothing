1357d7ad127e2c48d77d6f129681867fd8bbb43d
Paul Adrian <paulreyes74@yahoo.com>
2023-05-16 17:32:28 -0500

    refactor Category styles

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -5,7 +5,7 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Tuesday, 16th May 2023 4:13:37 pm
+ * Last Modified: Tuesday, 16th May 2023 5:28:39 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
@@ -13,12 +13,15 @@
  */
 
 import ProductCard from "../product-card/product-card.component";
+import { Link } from "react-router-dom";
 import './category-preview.styles.scss'
 const CategoryPreview = ({ title, products }) => {
     return (
         <div className="category-preview-container">
             <h2>
-                <span className="title">{title.toUpperCase()}</span>
+                <Link className='nav-link' to={title}>
+                    {title.toUpperCase()}
+                </Link>
             </h2>
             <div className="preview">
                 {products
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -5,14 +5,14 @@
  * Author: Paul Adrian Reyes (paulreyes74@yahoo.com)
  * GitHub: https://github.com/alcoranpaul
  * -----
- * Last Modified: Tuesday, 16th May 2023 4:29:41 pm
+ * Last Modified: Tuesday, 16th May 2023 5:31:32 pm
  * Modified By: PR (paulreyes74@yahoo.com>)
  * -----
  * -----
  * Description:
  */
 
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { CategoriesContext } from '../../contexts/categoriesMap.contexts';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
@@ -28,12 +28,16 @@ const Category = () => {
     }, [category, categoriesMap]); // Re-render when category or categoriesMap changes
 
     return (
-        <div className='category-container'>
-            {
-                products && products.map((product) => (
-                    <ProductCard key={product.id} product={product} />))
-            }
-        </div>
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {
+                    products && products.map((product) => (
+                        <ProductCard key={product.id} product={product} />))
+                }
+            </div>
+
+        </Fragment>
     );
 }
 
